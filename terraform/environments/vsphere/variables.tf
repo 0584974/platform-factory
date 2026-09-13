869bf10c998806060a63e430c7ba234eef3c2f2e
0584974 <43546487+0584974@users.noreply.github.com>
2026-09-13 10:50:43 +0300

    Add vSphere environment variables

---
+++ terraform/environments/vsphere/variables.tf
@@ -0,0 +1,32 @@
+variable "allow_unverified_ssl" {
+  type        = bool
+  description = "Allow an untrusted vCenter TLS certificate. Keep false in production."
+  default     = false
+}
+
+variable "datacenter" { type = string }
+variable "cluster" { type = string }
+variable "datastore" { type = string }
+variable "network" { type = string }
+
+variable "vm_folder" {
+  type        = string
+  description = "Existing vSphere VM folder. Null deploys to the default VM folder."
+  default     = null
+}
+
+variable "machines" {
+  description = "VMs cloned from existing vSphere templates. Templates should include VMware Tools and SSH/WinRM bootstrap."
+  type = map(object({
+    os_family   = string
+    template    = string
+    num_cpus    = number
+    memory_mb   = number
+    disk_size_gb = number
+  }))
+
+  validation {
+    condition     = alltrue([for machine in values(var.machines) : contains(["linux", "windows"], machine.os_family)])
+    error_message = "machines[*].os_family must be linux or windows."
+  }
+}
