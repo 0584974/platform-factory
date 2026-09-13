3d182e9c2b8729b01c39d49fd3964a03c09c356a
0584974 <43546487+0584974@users.noreply.github.com>
2026-09-13 10:50:13 +0300

    Add vSphere VM clone module

---
+++ terraform/modules/vsphere_vm/main.tf
@@ -0,0 +1,29 @@
+resource "vsphere_virtual_machine" "this" {
+  name             = var.name
+  resource_pool_id = var.resource_pool_id
+  datastore_id     = var.datastore_id
+  folder           = var.folder
+
+  num_cpus = var.num_cpus
+  memory   = var.memory_mb
+  guest_id = var.guest_id
+  scsi_type = var.scsi_type
+
+  wait_for_guest_ip_timeout  = var.wait_for_guest_ip_timeout
+  wait_for_guest_net_timeout = var.wait_for_guest_ip_timeout
+
+  network_interface {
+    network_id   = var.network_id
+    adapter_type = var.adapter_type
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.disk_size_gb
+    thin_provisioned = var.thin_provisioned
+  }
+
+  clone {
+    template_uuid = var.template_uuid
+  }
+}
