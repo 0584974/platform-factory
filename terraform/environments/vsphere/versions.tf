89156b51815d817863299c077391fa34cb11f133
0584974 <43546487+0584974@users.noreply.github.com>
2026-09-13 10:53:31 +0300

    Wire vSphere provider credentials through Terraform variables

--- terraform/environments/vsphere/versions.tf
+++ terraform/environments/vsphere/versions.tf
@@ -14,5 +14,8 @@ terraform {
 }
 
 provider "vsphere" {
+  vsphere_server       = var.vsphere_server
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
   allow_unverified_ssl = var.allow_unverified_ssl
 }
